2692a87cb5254a105e1e65c0e037708deac0a651
AbelCh <abelchocca1010@gmail.com>
2025-07-15 18:44:53 -0500

    Commit final

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ origins = ["http://localhost:5173", "https://pizzeria-frontend.up.railway.app"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins={"*"},
+    allow_origins={'*'},
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
